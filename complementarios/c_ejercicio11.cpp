3f6e999eeda122a43ee03d34d20c0d579b593bcf
daniharo <daniharopadul@gmail.com>
2019-03-10 13:51:36 +0100

    Ejercicio 11 terminado

--- complementarios/c_ejercicio11.cpp
+++ complementarios/c_ejercicio11.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <string.h>
 using namespace std;
 
 int calcularValor(const char cad[], int posIni, int posFin)
@@ -19,5 +20,22 @@ bool esValidoNIFNIE(const char id[], bool esNIF)
     const char control[] = "TRWAGMYFPDXBNJZSQVHLCKE";
     int valor = calcularValor(id, esNIF?0:1, 7);
 
-    return (id[8] == control[valor%23]);
+    return (toupper(id[8]) == control[valor%23]);
+}
+
+int main(void)
+{
+    const int TAM = 50;
+    char id[TAM];
+
+    do{
+        cout << "Introduzca un numero de identidad (NIF o NIE): ";
+        cin >> id;
+    } while(strlen(id) != 9 || !isalpha(id[8]));
+
+    bool esNIF = !isalpha(id[0]);
+
+    cout << "ID valido: " << (esValidoNIFNIE(id, esNIF) ? "SI" : "NO");
+
+    return (0);
 }
